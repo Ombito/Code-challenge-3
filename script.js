758bb87119335aab214d782dc1b561f503ece95f
ombito <alvinombito09@gmail.com>
2023-07-31 00:21:55 +0300

    Style movies page

--- script.js
+++ script.js
@@ -37,52 +37,15 @@ function showMovies (data) {
             <img src="${i.poster}">
             <h4>${i.title}</h4>
             <p>Runtime: ${i.runtime} minutes</p>
-            <p>Total capacity: ${i.capacity} minutes</p>
+            <p>Total capacity: ${i.capacity} </p>
             <p>Available ticket: ${availableTicket}</p>
             <p>Tickets Sold: ${i.tickets_sold} </p>
             <a id="ticketButton" href=#myForm><button>Buy Ticket</button>
         </card>
         `;
-
-    // function addButtons (btn){
-    // data.map(i => {
-    // if(availableTickets > 0) {
-    //     availableTickets--;
-    //     tickets_sold++;
-    //     }
-//  
-//     })
-//    } 
-// addButtons()
-// btn.addEventListener('click', console.log('I was clicked'))
+   
         movies.appendChild(newMovies);
         
-    //add a click event listener to the buy button
-    // button = document.getElementById("ticketButton")
-    // button.addEventListener('click', addTickets)
-         
-    // //add ticket function
-    // function addTickets(){
-    //     if(availableTickets > 0) {
-    //         availableTickets--;
-    //         tickets_sold++;
-    //     }
-    // }
-
-    //fetch to update the tickets details
-    // fetch("http://localhost:3000/films"), {
-    //     method: 'PUT',
-    //     headers: {
-    //         'content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({availableTickets, tickets_sold}),
-    // }
-    // .then((res) => Response.json())
-    // .then((data) => {
-    //     console.log(data)
-    // })
-
- 
 })
 
 }
